420d0fa9ca63e19db340ceb1d0fcd04363a53cd7
silverkiwi <adrian.white@gmail.com>
2023-03-04 15:58:40 +1300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ If you have any questions, checkout our [documentation](https://docs.streamlit.i
 forums](https://discuss.streamlit.io).
 
 In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+
 
 
 with st.echo(code_location='below'):
@@ -36,7 +36,7 @@ with st.echo(code_location='below'):
     st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
         .mark_circle(color='#0068c9', opacity=0.5)
         .encode(x='x:Q', y='y:Q'))
-
+"""
     
 import snowflake.connector
 
