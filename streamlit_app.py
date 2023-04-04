04139df7e401c18328aa8f7a511678fff319a8be
silverkiwi <adrian.white@gmail.com>
2023-04-04 17:28:01 +1200

    z

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,65 +1,82 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
+# The Snowpark package is required for Python Worksheets. 
+# You can add more packages by selecting them using the Packages control and then importing them.
+
+# import snowflake.snowpark as snowpark
+# Import required libraries
+from snowflake.snowpark.session import Session
+from snowflake.snowpark.functions import avg, sum, col,lit
 import streamlit as st
+import pandas as pd
 
-"""
-# Welcome to Streamlit!
+st.set_page_config(
+     page_title="Environment Data Atlas",
+     page_icon="🧊",
+     layout="wide",
+     initial_sidebar_state="expanded",
+     menu_items={
+         'Get Help': 'https://developers.snowflake.com',
+         'About': "This is an *extremely* cool app powered by Snowpark for Python, Streamlit, and Snowflake Data Marketplace"
+     }
+)
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+# Create Session object 
+def create_session_object():
+   connection_parameters = {
+      "account": "JK09545.ap-southeast-2",
+      "user": "SNOWBALL",
+      "password": "Banana123!",
+      "role": "ACCOUNTADMIN",
+      "warehouse": "COMPUTE_WH",
+      "database": "ENVIRONMENT_DATA_ATLAS",
+      "schema": "ENVIRONMENT"
+   }
+   session = Session.builder.configs(connection_parameters).create()
+   return session
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
-
+# Add header and a subheader
+st.header("Knoema: Environment Data Atlas")
+st.subheader("Powered by Snowpark for Python and Snowflake Data Marketplace | Made with Streamlit")
+  
+# Create Snowpark DataFrames that loads data from Knoema: Environmental Data Atlas
+def load_data(session):
+    # CO2 Emissions by Country
+    snow_df_co2 = session.table("ENVIRONMENT.EDGARED2019").filter(col('Indicator Name') == 'Fossil CO2 Emissions').filter(col('Type Name') == 'All Type')
+    snow_df_co2 = snow_df_co2.group_by('Location Name').agg(sum('$16').alias("Total CO2 Emissions")).filter(col('Location Name') != 'World').sort('Location Name')
     
-import snowflake.connector
+    # Forest Occupied Land Area by Country
+    snow_df_land = session.table("ENVIRONMENT.\"WBWDI2019Jan\"").filter(col('Series Name') == 'Forest area (% of land area)')
+    snow_df_land = snow_df_land.group_by('Country Name').agg(sum('$61').alias("Total Share of Forest Land")).sort('Country Name')
+    
+    # Total Municipal Waste by Country
+    snow_df_waste = session.table("ENVIRONMENT.UNENVDB2018").filter(col('Variable Name') == 'Municipal waste collected')
+    snow_df_waste = snow_df_waste.group_by('Location Name').agg(sum('$12').alias("Total Municipal Waste")).sort('Location Name')
+    
+    # Convert Snowpark DataFrames to Pandas DataFrames for Streamlit
+    pd_df_co2  = snow_df_co2.to_pandas()
+    pd_df_land = snow_df_land.to_pandas() 
+    pd_df_waste = snow_df_waste.to_pandas()
+    
+    # Use columns to display the three dataframes side-by-side along with their headers
+    col1, col2, col3 = st.columns(3)
+    with st.container():
+        with col1:
+            st.subheader('CO2 Emissions by Country')
+            st.dataframe(pd_df_co2)
+        with col2:
+            st.subheader('Forest Occupied Land Area by Country')
+            st.dataframe(pd_df_land)
+        with col3:
+            st.subheader('Total Municipal Waste by Country')
+            st.dataframe(pd_df_waste)
+    
+    # Display an interactive chart to visualize CO2 Emissions by Top N Countries
+    with st.container():
+        st.subheader('CO2 Emissions by Top N Countries')
+        with st.expander(""):
+            emissions_threshold = st.slider(label='Emissions Threshold',min_value=5000, value=20000, step=5000)
+            pd_df_co2_top_n = snow_df_co2.filter(col('Total CO2 Emissions') > emissions_threshold).to_pandas()
+            st.bar_chart(data=pd_df_co2_top_n.set_index('Location Name'), width=850, height=500, use_container_width=True)
 
-# Initialize connection.
-# Uses st.cache_resource to only run once.
-@st.cache_resource
-def init_connection():
-    return snowflake.connector.connect(
-        **st.secrets["snowflake"], client_session_keep_alive=True
-    )
-
-conn = init_connection()
-
-# Perform query.
-# Uses st.cache_data to only rerun when the query changes or after 10 min.
-@st.cache_data(ttl=600)
-def run_query(query):
-    with conn.cursor() as cur:
-        cur.execute(query)
-        return cur.fetchall()
-
-rows = run_query("SELECT top 10 * from h2;")
-
-# Print results.
-for row in rows:
-    st.write(f"{row[0]} has a :{row[1]}:")
+if __name__ == "__main__":
+    session = create_session_object()
+    load_data(session)
